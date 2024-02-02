69efa8203640e79abb45323326c0bd20c8074fcc
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2024-02-02 20:14:56 +0600

    connected store to the app

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../slice/api/apiSlice";
-
-const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
   reducer: {
