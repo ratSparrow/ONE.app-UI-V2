cc7b7db9ef30c8ffbddbee26594e4271750f9688
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2024-02-09 23:12:05 +0600

    added sub service modal

---
+++ src/components/modal/SubServiceModal.jsx
@@ -0,0 +1,158 @@
+/* eslint-disable react/prop-types */
+import { Button, Col, List, Modal, Row, Typography } from "antd";
+import { Link } from "react-router-dom";
+
+const SubServiceModal = ({ isModalOpen, handleOk, handleCancel, packages }) => {
+  console.log(packages);
+  return (
+    <Modal
+      title=<div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <h3 style={{ color: "blueviolet", fontWeight: 400 }}>
+          {packages.data[0].subServiceId.name}
+        </h3>
+        <h3 style={{ color: "blueviolet", fontWeight: 400 }}>
+          {packages.data.length} options available
+        </h3>
+      </div>
+      open={isModalOpen}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      style={{ width: 800 }}
+    >
+      <Row gutter={16} justify="space-around">
+        <Col className="gutter-row" span={24}>
+          <Row
+            gutter={16}
+            justify="space-around"
+            style={{
+              display: "flex",
+              margin: 8,
+              flexDirection: "column",
+            }}
+          >
+            {packages?.data.map((item) => (
+              <>
+                <Col
+                  style={{
+                    padding: 16,
+                    marginBottom: 16,
+                    borderRadius: "10px",
+                  }}
+                  key={item._id}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div>
+                      <img
+                        src={item.images[0]}
+                        style={{
+                          height: "80px",
+                          width: "90px",
+                          borderRadius: "12px",
+                        }}
+                        alt="item"
+                      />
+                    </div>
+                    {/*iMage part end */}
+
+                    <div>
+                      <List
+                        style={{
+                          width: "180px",
+                          border: "none",
+                        }}
+                        size="small"
+                        bordered
+                        dataSource={item.options}
+                        renderItem={(item) => (
+                          <List.Item style={{ fontSize: "11px" }}>
+                            {item}
+                          </List.Item>
+                        )}
+                      />
+                    </div>
+                    {/*Options  part end */}
+
+                    <div>
+                      <Typography
+                        style={{
+                          fontSize: "16px",
+                          fontWeight: 500,
+                          textAlign: "center",
+                        }}
+                      >
+                        {item.unit}
+                      </Typography>
+                      <List
+                        style={{
+                          width: "180px",
+                          border: "none",
+                        }}
+                        size="small"
+                        bordered
+                        dataSource={item.details}
+                        renderItem={(item) => (
+                          <List.Item style={{ fontSize: "11px" }}>
+                            {item}
+                          </List.Item>
+                        )}
+                      />
+                      <Typography
+                        style={{
+                          textAlign: "center",
+                          fontWeight: 600,
+                          color: "blueviolet",
+                          fontSize: "20px",
+                        }}
+                      >
+                        Cost - <span>{item.price}</span>
+                      </Typography>
+                    </div>
+                    {/*Details part end */}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "end",
+                      marginBottom: 12,
+                    }}
+                  >
+                    <Button
+                      style={{
+                        marginTop: 10,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                      size="small"
+                      type="primary"
+                    >
+                      <Link style={{}} to={`/checkout/${item._id}`}>
+                        {" "}
+                        <span>Add</span>{" "}
+                      </Link>
+                    </Button>
+                  </div>
+                  <hr />
+                </Col>
+              </>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Modal>
+  );
+};
+
+export default SubServiceModal;
---
+++ src/components/services/DetailsComponent.jsx
@@ -0,0 +1,364 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { format } from "date-fns";
+import "../../pages/css/ServiceDetails.css"
+import { Button, Card, Col, Row, Typography } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+
+const ServiceDetails = ({ detailsService, packages }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const originalDateStr = detailsService.createdAt;
+  const date = new Date(originalDateStr);
+
+  const formattedDate = format(date, "yyyy-MM-dd");
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div
+      className="mainDetails"
+      style={{ padding: "0 16px", marginTop: "32px" }}
+    >
+      <Row gutter={16} style={{ padding: "24px 0px" }} justify="space-between">
+        <Col className="gutter-row" span={12}>
+          <Typography
+            style={{
+              fontFamily: "serif",
+              color: "blueviolet",
+              fontSize: 32,
+              paddingBottom: 4,
+              marginTop: 16,
+              fontWeight: 600,
+            }}
+          >
+            {detailsService.name}
+          </Typography>
+          {/* END Title */}
+          <Typography
+            style={{
+              fontFamily: "serif",
+              fontWeight: 400,
+              fontSize: 18,
+              paddingBottom: 16,
+              color: "blueviolet",
+              marginBottom: 16,
+            }}
+          >
+            Category by {detailsService.serviceId.name}
+          </Typography>{" "}
+          {/* END Category */}
+          <Typography style={{ fontFamily: "serif", paddingBottom: 8 }}>
+            <span
+              style={{
+                boxShadow:
+                  "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+                padding: 10,
+                borderRadius: 6,
+                color: "blueviolet",
+              }}
+            >
+              Warranty {detailsService.warrantyHour} years
+            </span>
+            <span
+              style={{
+                boxShadow:
+                  "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+                padding: 10,
+                borderRadius: 6,
+                marginLeft: 4,
+                color: "blueviolet",
+              }}
+            >
+              No Discount
+            </span>
+          </Typography>
+          {/* END Warranty and Discount  */}
+          <Typography style={{ fontFamily: "serif", marginTop: "16px" }}>
+            <span
+              style={{
+                boxShadow:
+                  "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+                padding: 10,
+                borderRadius: 6,
+                color: "blueviolet",
+              }}
+            >
+              Total Rating {detailsService.totalRating}
+            </span>
+            <span
+              style={{
+                boxShadow:
+                  "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+                padding: 10,
+                borderRadius: 6,
+                color: "blueviolet",
+                marginLeft: 4,
+              }}
+            >
+              Since {formattedDate}
+            </span>
+          </Typography>
+          {/* END Warranty and Discount  */}
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Card
+            style={{
+              maxWidth: 400,
+              boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+              marginTop: "24px",
+            }}
+          >
+            {packages?.data?.map((item) => (
+              <div style={{}} key={item._id}>
+                <Button
+                  style={{ width: "100%" }}
+                  onClick={showModal}
+                  className="cardModalBtn"
+                >
+                  <span style={{ color: "blueviolet" }}>
+                    {" "}
+                    {item.name.slice(0, 25)}..
+                  </span>
+                  <span style={{ color: "blueviolet", fontWeight: 700 }}>
+                    <RightOutlined />
+                  </span>
+                </Button>
+                <SubServiceModal
+                  isModalOpen={isModalOpen}
+                  handleOk={handleOk}
+                  handleCancel={handleCancel}
+                  packages={packages}
+                />
+              </div>
+            ))}
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{ padding: "20px 0px" }}>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <span
+              style={{
+                backgroundColor: "blueviolet",
+                padding: 10,
+                borderRadius: 6,
+                display: "inline-block",
+                color: "white",
+                fontFamily: "serif",
+                fontSize: 16,
+                marginTop: 24,
+                marginBottom: "-16px",
+              }}
+            >
+              Description
+            </span>
+            <Typography
+              style={{
+                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                padding: 12,
+                borderRadius: 8,
+              }}
+            >
+              <br />
+              <span
+                style={{
+                  fontFamily: "serif",
+                }}
+              >
+                {detailsService.description}
+              </span>
+            </Typography>
+          </div>
+          {/* END Description  */}
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <span
+              style={{
+                backgroundColor: "blueviolet",
+                padding: 10,
+                borderRadius: 6,
+                display: "inline-block",
+                color: "white",
+                fontFamily: "serif",
+                fontSize: 16,
+                marginTop: 24,
+                marginBottom: "-18px",
+              }}
+            >
+              Features
+            </span>
+            <Typography
+              style={{
+                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                padding: 12,
+                borderRadius: 8,
+              }}
+            >
+              <br />
+
+              {detailsService.features.map((feature, index) => (
+                <span
+                  style={{ display: "block", fontFamily: "serif" }}
+                  key={index}
+                >
+                  <CheckCircleOutlined
+                    style={{ color: "blueviolet", marginRight: 8 }}
+                  />
+                  {feature}
+                </span>
+              ))}
+            </Typography>
+          </div>
+          {/* END Features  */}
+        </Col>
+      </Row>
+      <Row gutter={16} style={{ padding: "20px 0px" }}>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <span
+              style={{
+                backgroundColor: "blueviolet",
+                padding: 10,
+                borderRadius: 6,
+                display: "inline-block",
+                color: "white",
+                fontFamily: "serif",
+                fontSize: 16,
+                marginTop: 24,
+                marginBottom: "-18px",
+              }}
+            >
+              Included
+            </span>
+            <Typography
+              style={{
+                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                padding: 12,
+                borderRadius: 8,
+              }}
+            >
+              <br />
+
+              {detailsService.includedOption.map((feature, index) => (
+                <span
+                  style={{ display: "block", fontFamily: "serif" }}
+                  key={index}
+                >
+                  <CheckCircleOutlined
+                    style={{ color: "blueviolet", marginRight: 8 }}
+                  />
+                  {feature}
+                </span>
+              ))}
+            </Typography>
+          </div>{" "}
+          {/* END Included  */}
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <span
+              style={{
+                backgroundColor: "blueviolet",
+                padding: 10,
+                borderRadius: 6,
+                display: "inline-block",
+                color: "white",
+                fontFamily: "serif",
+                fontSize: 16,
+                marginTop: 24,
+                marginBottom: "-18px",
+              }}
+            >
+              Excluded
+            </span>
+            <Typography
+              style={{
+                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                padding: 12,
+                borderRadius: 8,
+              }}
+            >
+              <br />
+
+              {detailsService.excludedOption.map((feature, index) => (
+                <span
+                  style={{ display: "block", fontFamily: "serif" }}
+                  key={index}
+                >
+                  <CloseCircleOutlined
+                    style={{ color: "blueviolet", marginRight: 8 }}
+                  />
+
+                  {feature}
+                </span>
+              ))}
+            </Typography>
+          </div>{" "}
+          {/* END Excluded Part  */}
+        </Col>
+      </Row>
+      <div>
+        <span
+          style={{
+            backgroundColor: "blueviolet",
+            padding: 10,
+            borderRadius: 6,
+            display: "inline-block",
+            color: "white",
+            fontFamily: "serif",
+            fontSize: 16,
+            marginTop: 24,
+            marginBottom: "-18px",
+          }}
+        >
+          Terms And Conditions
+        </span>
+        <div
+          style={{
+            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+            padding: 12,
+            borderRadius: 8,
+          }}
+        >
+          <br />
+
+          {detailsService.termsCondition.map((feature, index) => (
+            <Typography key={index}>
+              <span
+                style={{
+                  fontSize: 18,
+                  fontWeight: 600,
+                  color: "blueviolet",
+                  fontFamily: "serif",
+                }}
+              >
+                {feature?.title}:{" "}
+              </span>
+              <span
+                style={{
+                  fontSize: 15,
+                  fontWeight: 400,
+
+                  fontFamily: "serif",
+                }}
+              >
+                {feature?.details}
+              </span>
+            </Typography>
+          ))}
+        </div>
+      </div>{" "}
+      {/* END Terms and condition  */}
+    </div>
+  );
+};
+
+export default ServiceDetails;
